9d00c6f2f041c66cfb6d90eca9dfb375d52160ec
Manjot <manjotvirk705@gmail.com>
2022-08-07 20:54:31 +0530

    Committed 7August2022

--- 8.5August2022/sq.js
+++ 8.5August2022/sq.js
@@ -1,28 +1,23 @@
 function createSq(){
+    document.getElementById("ans").style.width="0px";
+    document.getElementById("ans").style.display="block";
     var n=document.getElementById("num").value;
     var p=n;
     var sum=0;
     const len=p.toString().length;
-    // alert(len);
     if(len!=5)alert("Please enter the 5 digit number");
     else{
         while(n!=0){
             sum=sum+(n%10);
             n=parseInt(n/10)
         }
-        sq(sum);
         var ll=sum/5;
-        // alert(ll);
-        document.getElementById("ans").innerHTML="<h1>ll</h1>";
+        document.getElementById("ans").style.width=ll+"vw";
+        document.getElementById("ans").style.height=ll+"vw";
     }
-}
+};
 
-function sq(sum){
-
-}
-
-// const res=document.getElementById("ans");
-// res.innerHTML="hi i am res";    
-
-// document.getElementById("ans").style.width="300px";
-// document.getElementById("ans").style.height="300px";
+function reset(){
+    document.getElementById("num").value="";
+    document.getElementById("ans").style.display="none";
+};
